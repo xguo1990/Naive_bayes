9b5636e0fa42f4688b0cedfa08ad927e87472def
Xiang Guo <xguo1990@gmail.com>
2017-06-30 22:24:24 -0400

    Use the train data to train the classifier (clf) and use the trained model to make pridictions on the test data. And validation.

--- nb_author_id.py
+++ nb_author_id.py
@@ -21,8 +21,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "training time:", round(time()-t0, 3), "s"
 
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
 
 #########################################################
 ### your code goes here ###
